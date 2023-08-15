eaba1dea11c71f13dae474b5a123cd90d032ed87
Derkach Oleksandr <derkach03553@gmail.com>
2023-08-15 12:39:22 +0300

    refactoring

---
+++ README.md
@@ -0,0 +1,7 @@
+Ajax Test Task
+
+[Tasks](task.pdf)
+
+[Solution for task 1](main.py)
+
+[Solution for task 2](test_scanner_handler.py)
---
+++ device/device.py
@@ -0,0 +1,41 @@
+class Device:
+    def __init__(self, device_id: str):
+        self.device_id = device_id
+        self.states_count = 0
+        self.error_messages = []
+
+    def check_state(self, state: str, sp1: str, sp2: str) -> None:
+        if state == '02':
+            if self.states_count is not None:
+                self.states_count += 1
+        else:
+            self.states_count = None
+            self.set_error_message(sp1, sp2)
+
+    def set_error_message(self, sp1: str, sp2: str) -> None:
+        errors = {
+            1: 'Battery device error',
+            2: 'Temperature device error',
+            3: 'Threshold central error'
+        }
+        state = sp1[:-1] + sp2
+        # split previous result into pairs and convert it to binary form
+        errors_binaries = [bin(int(state[i:i + 2])) for i in range(0, len(state), 2)]
+        # of the resulting bit fields, leaves only fifth flags
+        device_errors = [i[-4] if len(i) > 5 else 0 for i in errors_binaries]
+        # mapping errors with flags
+        messages = [errors[i + 1] for i in range(3) if device_errors[i] == '1']
+
+        for message in messages:
+            if message not in self.error_messages:
+                self.error_messages.append(message)
+
+    def get_error_message(self):
+        message = ', '.join(self.error_messages) if self.error_messages else 'Unknown device error'
+        return f'{self.device_id}: {message}'
+
+    def __str__(self):
+        if self.states_count is not None:
+            return f'{self.device_id}: {self.states_count}'
+        else:
+            return f'{self.device_id}: {self.get_error_message()}'
